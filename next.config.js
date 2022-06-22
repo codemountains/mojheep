8c764a6dabc1e4a942e8a51991e43f4ebea5923b
codemountains <4kz12zz@gmail.com>
2022-06-23 01:46:21 +0900

    :green_heart: fix build settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    experimental: {
+        nftTracing: true
+    },
     webpack: (config, {isServer}) => {
         config.experiments = {
             asyncWebAssembly: true
